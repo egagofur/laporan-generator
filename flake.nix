580b1b17f49d89ed1c94edbfb4c3124db1ccb79c
Mu'adz Hudzaifah <muadzhudzaifah511@gmail.com>
2026-08-19 09:05:28 +0700

    feat: add Nix flake devShell with pandoc, texlive, typst & tooling

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Laporan Generator - development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            collection-basic
+            collection-latex
+            collection-latexrecommended
+            collection-latexextra
+            collection-fontsrecommended
+            collection-langother;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "laporan-generator";
+          packages = [
+            pkgs.pandoc
+            tex
+            pkgs.imagemagick
+            pkgs.gnumake
+            pkgs.coreutils
+            pkgs.inotify-tools
+            pkgs.xdg-utils
+            pkgs.poppler-utils
+            pkgs.haskellPackages.pandoc-crossref
+            pkgs.shellcheck
+            pkgs.typst
+          ];
+        };
+      });
+}
